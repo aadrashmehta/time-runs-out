6d957c3f40a5f6dab0a2ef0012404ef7a762e057
Aadrash Mehta <mehta.aadrash05@gmail.com>
2025-11-06 11:46:20 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useState, useRef } from 'react'
+import Player from './components/Player.jsx';
+
+function App() {
+  // const inputValue = useRef();
+  // const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Player />
+      <div id="challenge"></div>
+{/* 
+    <div className="card">
+      <button onClick={() => setCount((count) => count > 1 ? count - 1 : 1)} disabled={count < 1 ? true: false}>
+        -
+      </button>
+      <input 
+        type="number" 
+        ref={inputValue} 
+        value={count} 
+        min="1"
+        onChange={(e) => setCount(Number(e.target.value))}
+      />
+      <button onClick={() => setCount((count) => count + 1)}>
+        +
+      </button>
+      <p>
+        {count > 1 &&
+          `Edit <code>src/App.jsx</code> and save to test HMR`}
+        {count <= 1 &&   
+          `Count can't be less than 1`
+        }
+      </p>
+      <button onClick={() => inputValue.current.focus()}>
+        Focus
+      </button>
+    </div>
+*/}
+    </>
+  )
+}
+
+export default App
